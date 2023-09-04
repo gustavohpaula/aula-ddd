5d2c44a668641eff4706024bf8f1928f3e7ed10e
Gustavo <gustavohp4@gmail.com>
2023-09-04 15:41:19 -0300

    correção no console do endereço

--- src/domain/event/customer/handler/send-console-log-when-address-change.handler.ts
+++ src/domain/event/customer/handler/send-console-log-when-address-change.handler.ts
@@ -4,6 +4,8 @@ import AddressChangedEvent from "../address-changed.event";
 
 export default class sendConsoleLogWhenAddressChange implements EventHandlerInterface<AddressChangedEvent>{
     handle(event: AddressChangedEvent): void {
-        console.log(`Endereço do cliente: ${event.eventData.id}, ${event.eventData.name} alterado para: ${event.eventData.address}`);
+        console.log(`Endereço do cliente: ${event.eventData.id}, ${event.eventData.name} 
+        alterado para: ${event.eventData.address.street}, ${event.eventData.address.number},
+         ${event.eventData.address.zip}, ${event.eventData.address.city}`);
     }
 }
