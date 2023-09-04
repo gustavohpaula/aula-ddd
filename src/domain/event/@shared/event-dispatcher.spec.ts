bd3e09f4b97a534bab2ff5483ed9ae6efbb14ac7
Gustavo <gustavohp4@gmail.com>
2023-09-04 14:28:34 -0300

    desregistrando eventos

--- src/domain/event/@shared/event-dispatcher.spec.ts
+++ src/domain/event/@shared/event-dispatcher.spec.ts
@@ -1,3 +1,4 @@
+import { LIMIT_EXPR_DEPTH } from "sqlite3";
 import SendEmailWhenProductIsCreatedHandler from "../product/handler/send-email-when-product-is-created.handler";
 import EventDispatcher from "./event-dispatcher";
 
@@ -11,5 +12,38 @@ describe("Domain events tests", () => {
 
         expect(eventDispatcher.getEventHandlers["ProductCreatedEvent"]).toBeDefined();
         expect(eventDispatcher.getEventHandlers["ProductCreatedEvent"].length).toBe(1);
+
+        expect(eventDispatcher.getEventHandlers["ProductCreatedEvent"][0]).toMatchObject(eventHandler);
+    })
+
+    it("should unregister an event handler", () => {
+
+        const eventDispatcher = new EventDispatcher();
+        const eventHandler = new SendEmailWhenProductIsCreatedHandler();
+
+        eventDispatcher.register("ProductCreatedEvent", eventHandler);
+
+        expect(eventDispatcher.getEventHandlers["ProductCreatedEvent"][0]).toMatchObject(eventHandler);
+
+        eventDispatcher.unregister("ProductCreatedEvent", eventHandler);
+
+        expect(eventDispatcher.getEventHandlers["ProductCreatedEvent"]).toBeDefined();
+        expect(eventDispatcher.getEventHandlers["ProductCreatedEvent"].length).toBe(0);
+
+    })
+
+    it("should unregister all event handlers", () => {
+
+        const eventDispatcher = new EventDispatcher();
+        const eventHandler = new SendEmailWhenProductIsCreatedHandler();
+
+        eventDispatcher.register("ProductCreatedEvent", eventHandler);
+
+        expect(eventDispatcher.getEventHandlers["ProductCreatedEvent"][0]).toMatchObject(eventHandler);
+
+        eventDispatcher.unregisterAll();
+
+        expect(eventDispatcher.getEventHandlers["ProductCreatedEvent"]).toBeUndefined();
+
     })
 })
