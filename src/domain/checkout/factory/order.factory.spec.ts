d75a71c64d1066f5352019a5fe2f55d1d597f3df
Gustavo <gustavohp4@gmail.com>
2023-09-05 15:51:16 -0300

    finalização do projeto

--- src/domain/checkout/factory/order.factory.spec.ts
+++ src/domain/checkout/factory/order.factory.spec.ts
@@ -1,4 +1,5 @@
 import { v4 as uuid } from "uuid"
+import OrderFactory from "./order.factory";
 
 describe("Order factory unit test", () => {
     it("should create an order", () => {
@@ -18,7 +19,7 @@ describe("Order factory unit test", () => {
         const order = OrderFactory.create(orderProps);
 
         expect(order.id).toEqual(orderProps.id);
-        expect(order.customer_id).toEqual(orderProps.customerId);
+        expect(order.customerId).toEqual(orderProps.customerId);
         expect(order.items.length).toBe(1);
     })
 })
